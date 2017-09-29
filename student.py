69ed6fbcfce038798963f3b77ea6338aaed3fee6
davisb <brianpatrick.brian517@gmail.com>
2017-09-29 14:57:42 -0400

    dancing yaaaay

--- student.py
+++ student.py
@@ -57,27 +57,31 @@ class Piggy(pigo.Pigo):
         """executes a series of methods that add up to a compound dance"""
         print("\n---- LET'S DANCE ----\n")
         ##### WRITE YOUR FIRST PROJECT HERE
-        #self.to_the_right()
-        #self.to_the_left()
-        #self.now_kick()
-        #self.cha_cha()
+        self.to_the_right()
+        self.to_the_left()
+        self.now_kick()
+        self.cha_cha()
         self.walk_it_by_yourself()
 
     def to_the_right(self):
         """To the right"""
         for x in range(4):
+            self.servo(73)
             self.encR(10)
             self.encF(5)
             self.encR(10)
             self.encB(5)
+            self.servo(103)
 
     def to_the_left(self):
         """To the left"""
         for x in range(4):
+            self.servo(133)
             self.encL(10)
             self.encB(5)
             self.encL(10)
             self.encF(5)
+            self.servo(103)
 
     def now_kick(self):
         """Now kick"""
@@ -104,6 +108,7 @@ class Piggy(pigo.Pigo):
                 self.encF(18)
             else:
                 self.encB(18)
+            self.servo(103)
 
 
 
