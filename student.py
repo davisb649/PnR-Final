2c0c8355bf447e83d4f428f96dc0821bad6671ec
davisb <brianpatrick.brian517@gmail.com>
2017-11-06 15:00:11 -0500

    frustration II electric frustraloo

--- student.py
+++ student.py
@@ -235,7 +235,9 @@ class Piggy(pigo.Pigo):
 
     def rotation_testing(self):
         """Just testing how strong the motors are by rotating until i hit 360 deg"""
-        self.encR(35)
+        self.encR(10)
+        self.encR(10)
+        self.encR(10)
 
     def restore_heading (self):
         """turns back the way i'm supposed to be going by using self.turn_track()"""
