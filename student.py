33ad768dcf8b3a204bcd368e241fe75231ac63e6
davisb <brianpatrick.brian517@gmail.com>
2017-11-08 14:25:20 -0500

    i hate rotation testing

--- student.py
+++ student.py
@@ -237,7 +237,7 @@ class Piggy(pigo.Pigo):
 
     def rotation_testing(self):
         """Just testing how strong the motors are by rotating until i hit 360 deg"""
-        self.encR(35)
+        self.encR(36)
 
     def restore_heading(self):
         """turns back the way i'm supposed to be going by using self.turn_track()"""
