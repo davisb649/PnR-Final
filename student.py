f3b1e656464c590c7c6a967d5204a510daafc3ae
davisb <brianpatrick.brian517@gmail.com>
2017-12-01 14:48:36 -0500

    wheel speed fixing

--- student.py
+++ student.py
@@ -24,7 +24,7 @@ class Piggy(pigo.Pigo):
         self.SAFE_STOP_DIST = 30
         self.HARD_STOP_DIST = 30
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.LEFT_SPEED = 125
+        self.LEFT_SPEED = 123
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.RIGHT_SPEED = 130
         # This one isn't capitalized because it changes during runtime, the others don't
